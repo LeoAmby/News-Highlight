6dc68517770406bce0037471e43ff4e0ab1035d9
Leo <leoamby@gmail.com>
2019-09-09 12:31:57 +0300

    import flask app to the relevant files and countering the news test file by ensuring it passes

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    '''
+    Defining News Objects
+    '''
+
+    def __init__ (self,publishers, countries, Categories):
+        self.publisher = publishers
+        self.country = country
+        self.categories = Categories
