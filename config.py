4a8ed8f967dfa9b7cc70c59b55a1a2c3483d97e9
Leo <leoamby@gmail.com>
2019-09-09 15:54:02 +0300

    this commit will update the relevant files

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     These is the General configuration parent class
     '''
 
-    NEWS_API_BASE_URL = 'https://newsapi.org/sources/{}?api_key={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?q={}&apiKey={}'
