fcd13efc2c9f8b76ff01dec37a333d9b75d41c24
Leo <leoamby@gmail.com>
2019-09-09 17:46:52 +0300

    initialize bootstrap extentions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #initializing application
 app = Flask(__name__, instance_relative_config = True)
@@ -8,5 +9,7 @@ app = Flask(__name__, instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+#Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
