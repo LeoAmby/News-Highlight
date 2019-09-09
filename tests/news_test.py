4fd82d1a646749e84c7701baf01621620aa3610d
Leo <leoamby@gmail.com>
2019-09-09 15:26:10 +0300

    more files

---
+++ tests/news_test.py
@@ -0,0 +1,24 @@
+import unittest
+from models import news
+News = news.News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Testing the functionality of the News class
+    '''
+
+    def setUp(self):
+        '''
+        this defined 'setup' test is made to run before every test
+        '''
+
+        self.new_news = News('our news highlight flask app')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news, News))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
