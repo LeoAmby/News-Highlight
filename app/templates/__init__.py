0f42afdcf54ee80d4a30a2990c599c0e655e50e4
Leo <leoamby@gmail.com>
2019-09-09 15:32:28 +0300

    this commit will reposition the __init__.py file

---
+++ app/templates/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .config import DevConfig
+
+#initializing application
+app = Flask(__name__, instance_relative_config = True)
+
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+
+from app import views
