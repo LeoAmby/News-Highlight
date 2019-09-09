7ece618cf588a24ce66218c68bc5954b125de522
Leo <leoamby@gmail.com>
2019-09-09 15:33:19 +0300

    this commit will delete unnecessary files for the root

--- app/models/news.py
+++
@@ -1,9 +0,0 @@
-class News:
-    '''
-    Defining News Objects
-    '''
-
-    def __init__ (self,publishers, countries, categories):
-        self.publisher = publishers
-        self.country = country
-        self.categories = categories
