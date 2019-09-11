53a02555959a16ca68963c51993721ecda35f210
Leo <leoamby@gmail.com>
2019-09-11 13:00:21 +0300

    update files

--- app/main/errors.py
+++ app/main/errors.py
@@ -8,4 +8,4 @@ def four_Ow_four(error):
     Function for rendering the 404 error page
     '''
     title = 'Error 404'
-    return render_template('fourOwfour.html, title = title'),404
+    return render_template('404.html', title = title),404
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
     '''
     These is the General configuration parent class
